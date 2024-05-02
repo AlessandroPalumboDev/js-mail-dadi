e167e708fe41f58ce59819e92fee7291cba28196
AlessandroPalumboDev <alessandropalumbo.dev@gmail.com>
2024-05-02 19:35:06 +0200

    [UPD] Creo condizioni di vittoria e stampo

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -2,10 +2,19 @@
 
 // Creo variabile del pulsante con evento al click
 const lancio = document.getElementById("lancio-input");
-genera.addEventListener('click', function () {
+lancio.addEventListener('click', function () {
 
     // Genero numeri casuali del dado utente e pc
-    const userNumber = Math.floor(Math.random() * 6);
-    const computerNumber = Math.floor(Math.random() * 6);
+    const userNumber = Math.floor((Math.random() * 6) + 1);
+    const computerNumber = Math.floor((Math.random() * 6) + 1);
 
+    // --- Creo IF per vittoria/pareggio altrimenti sconfitta e stampo risultato
+    if(userNumber === computerNumber){
+        document.getElementById('risultato-output').innerHTML = ('PAREGGIO')
+    }else if(userNumber > computerNumber){
+        document.getElementById('risultato-output').innerHTML = ('HAI VINTO!')
+    }else{
+        document.getElementById('risultato-output').innerHTML = ('HAI PERSO!')
+    };
+    document.getElementById('spiegazione-output').innerHTML = (`Il tuo dado ha fatto ${userNumber}, il mio dato ha fatto ${computerNumber}`)
 });
