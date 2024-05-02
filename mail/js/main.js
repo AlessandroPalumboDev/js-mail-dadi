eeb5f9e4d374f2acbbc5d031e03f17b7de77b2f8
AlessandroPalumboDev <alessandropalumbo.dev@gmail.com>
2024-05-02 18:31:55 +0200

    [UPD] Condizioni per l'accesso

--- mail/js/main.js
+++ mail/js/main.js
@@ -9,3 +9,12 @@ const emailList = ["pincopallino@mail.com"];
 for (let email = 1; email < 10; email++){
     emailList.push(`pinco${emailList.length}pallino@mail.com`)
 };
+
+// --Creo condizioni per l'accesso e stampo gli eventuali messaggi
+if (userEmail === emailList[0] || userEmail === emailList[1] || userEmail === emailList[2] || userEmail === emailList[3] || userEmail === emailList[4] || userEmail === emailList[5] || userEmail === emailList[6] || userEmail === emailList[7] || userEmail === emailList[8] || userEmail === emailList[9]){
+    console.log("Accesso Consentito");
+} else{
+    console.log('Accesso Negato')
+}
+
+// RIGA 14 MI UCCIDE!
